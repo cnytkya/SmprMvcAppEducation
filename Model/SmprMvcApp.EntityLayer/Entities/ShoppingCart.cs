872f7ddef97f34551049b8748595c75585581eb6
gizemdnm <gizem.34_@hotmail.com>
2024-12-22 10:58:34 +0300

    Added ShoppingCart

---
+++ Model/SmprMvcApp.EntityLayer/Entities/ShoppingCart.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmprMvcApp.EntityLayer.Entities
+{
+    public class ShoppingCart
+    {
+
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        public Product Product { get; set; }
+        [Range(1,100, ErrorMessage ="Please enter a value between 1 and 100")]
+        public int Count { get; set; }
+        public int UserId { get; set; }
+        [ForeignKey("UserId"), ValidateNever]
+        public AppUser AppUser { get; set; }
+    }
+}
