999fa06432960fa4206c3ac4d7eca4463fa438e2
cnytkya <kayacuneyt2542@gmail.com>
2024-12-29 11:40:20 +0300

    Added ShoppingCart CRUD Operations.

---
+++ Model/SmprMvcApp.EntityLayer/ViewModels/ShoppinCartViewModel.cs
@@ -0,0 +1,10 @@
+﻿using SmprMvcApp.EntityLayer.Entities;
+
+namespace SmprMvcApp.EntityLayer.ViewModels
+{
+    public class ShoppinCartViewModel
+    {
+        public IEnumerable<ShoppingCart> ShoppingCartList { get; set; }
+        public double OrderTotal { get; set; }
+    }
+}
